6b9b3a32025a18c2abc211cd7e9d92a6edbc84d1
Paweł Reliński <pawelrelinski@gmail.com>
2020-05-25 15:06:29 +0200

    Create navigation and footer component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HomeModule} from "./modules/home/home.module";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './core/navigation/navigation.component';
+import {FooterComponent} from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/core/navigation/navigation.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor() {
+  }
+
+  menuIsActive: boolean = false;
+
+  toggleMenu(): void {
+    this.menuIsActive = !this.menuIsActive;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
